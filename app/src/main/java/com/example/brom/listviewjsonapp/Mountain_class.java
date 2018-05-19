7b51ec576afa6bd8eb3c0db445ba041c17256c2d
b17annni <b17annni@gmail.com>
2018-05-19 18:43:38 +0200

    this is new

--- app/src/main/java/com/example/brom/listviewjsonapp/Mountain_class.java
+++ app/src/main/java/com/example/brom/listviewjsonapp/Mountain_class.java
@@ -9,33 +9,30 @@ public class Mountain_class {
     private String location;
     private int height;
 
-public Mountain_class (String inName, String inLocation, int inHeight){
-    name=inName;
-    location=inLocation;
-    height=inHeight;
-}
+    public Mountain_class (String inName, String inLocation, int inHeight){
+        name=inName;
+        location=inLocation;
+        height=inHeight;
+    }
 
-public Mountain_class(String inName){
-    name=inName;
-    location="";
-    height=-1;
-}
+    public Mountain_class(String inName){
+        name=inName;
+        location="";
+        height=-1;
+    }
 
-public String myStrings (){return name;}
-public String info(){
-    String str=name;
-    str+=" is dislocated in ";
-    str+=location;
-    str+=" and  is ";
-    str+=Integer.toString(height);
-    str+="m ";
-    return str;
+    public String myStrings (){return name;}
+    public String info(){
+        String str=location;
+        str+="  ";
+        str+=name;
+        return str;
 
-}
+    }
 
-public void setHeight(int newHeight){
-    height=newHeight;
-}
+    public void setHeight(int newHeight){
+        height=newHeight;
+    }
     @Override
     public String toString() {
         return name;
